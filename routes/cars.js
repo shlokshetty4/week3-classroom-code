08337c8462a9f8ad044a38978227e54d272ded8c
cherimarie <cherimarie@gmail.com>
2026-04-14 09:54:57 -0700

    Fetching car by id

--- routes/cars.js
+++ routes/cars.js
@@ -3,7 +3,20 @@ var router = express.Router();
 const carData = require('../data/cars.json')
 
 router.get('/', (req, res) => {
-  res.send(carData[0])
+  console.log(req.query.id)
+  if(!req.query.id){
+    res.send({message: "Please send an id as a query parameter"});
+    return;
+  }
+
+  let theCar = carData.find((el)=> el.id == req.query.id )
+
+  if(!theCar){
+    res.send({message: "There is no car with that id"});
+    return;
+  }
+
+  res.send(theCar);
 })
 
 module.exports = router;
